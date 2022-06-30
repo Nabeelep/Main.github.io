163afd08c643c3842a4e61cdee90581c862d0cef
Nabeelep <84263179+Nabeelep@users.noreply.github.com>
2022-06-30 16:19:01 +0530

    fordeploymnt

---
+++ src/components/Details/styles.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core'
+
+export default makeStyles(() => ({
+    income: {
+        borderBottom : '10px solid rgba(0, 225, 0, 0.5)',
+        // backgroundColor: '#25282c',
+        // color: '#dde0e3',
+        //  borderRadius: '15px'
+
+    },
+    expense: {
+        borderBottom : '10px solid rgba(225, 0, 0, 0.5)',
+        // backgroundColor: '#25282c',
+        // color: '#dde0e3',
+        // borderRadius: '15px'
+        
+    }
+}))
---
+++ src/components/Main/styles.js
@@ -0,0 +1,36 @@
+import { makeStyles } from '@material-ui/core/styles';
+import '../../assets/fonts/DejaVuSansMono.ttf'
+
+export default makeStyles((theme) => ({
+
+  root: {
+    //borderBottom : '10px solid #a9abb3',
+    borderBottom : '10px solid rgba(225, 0, 0, 0.5)',
+    // backgroundColor: '#25282c',
+    // color: '#dde0e3',
+    // borderRadius: '15px'
+  },
+
+  
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+  },
+  expand: {
+    transform: 'rotate(0deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
+  },
+  cardContent: {
+    paddingTop: 0,
+   
+  },
+  divider: {
+    margin: '20px 0',
+  },
+}));
